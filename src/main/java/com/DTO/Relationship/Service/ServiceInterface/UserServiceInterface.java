5af3989c76b3532a888a926913d15fe9969195f2
Ankit <ankitsharma.as420@gmail.com>
2025-03-27 16:39:52 +0530

    added multipart variable

--- src/main/java/com/DTO/Relationship/Service/ServiceInterface/UserServiceInterface.java
+++ src/main/java/com/DTO/Relationship/Service/ServiceInterface/UserServiceInterface.java
@@ -2,11 +2,13 @@ package com.DTO.Relationship.Service.ServiceInterface;
 
 import com.DTO.Relationship.Entity.User;
 import com.DTO.Relationship.Model.UserModel;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface UserServiceInterface {
-    public String saveUser(UserModel userModel);
+    public String saveUser(UserModel userModel, MultipartFile imageFile) throws IOException;
     public List<UserModel> getUserList();
     public UserModel findUserDetails(Integer userId);
 }
