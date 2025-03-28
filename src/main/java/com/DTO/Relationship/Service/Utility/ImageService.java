2a369f1896e97bc0c1115d7d59b30aee9c3e019e
Ankit <ankitsharma.as420@gmail.com>
2025-03-28 15:57:55 +0530

    created new method for get an encoded image from directory

--- src/main/java/com/DTO/Relationship/Service/Utility/ImageService.java
+++ src/main/java/com/DTO/Relationship/Service/Utility/ImageService.java
@@ -9,11 +9,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Base64;
 
 @Service
 public class ImageService {
     @Value("${file.upload-dir}")
     private String uploadDir;
+
+/** to saved the image in Directory and return the image file name */
+
     public String saveUserImage(MultipartFile imageFile) throws IOException {
         if(imageFile.isEmpty()){
             throw new IOException("Empty file can not save.");
@@ -26,7 +30,15 @@ public class ImageService {
         Files.createDirectories(directoryPath.getParent());
 //        save the image
         Files.copy(imageFile.getInputStream(),directoryPath, StandardCopyOption.REPLACE_EXISTING);
-
         return imageFileName;
+    }
+
+/** to retrieve the image from Directory*/
+    public String getEncodedImageFromDirectory(String imageFileName) throws IOException {
+        Path directoryPath = Paths.get(uploadDir).resolve(imageFileName);
+//                         = src/main/resources/user-image/464654967443146464_image.png
+        byte[] imageBytes = Files.readAllBytes(directoryPath); // files read all buyts and return byte[] Arrays
+        String encodedImage = Base64.getEncoder().encodeToString(imageBytes);// converting bytes Arrays into encoded String
+        return "data: image/jpeg:base64"+ encodedImage;
     }
 }
